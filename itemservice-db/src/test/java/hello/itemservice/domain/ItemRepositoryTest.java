afd27c1fc0f31e02b028056b4e1bbf40fa6072ad
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-05-07 23:00:48 +0900

    feat : 테스트 - @Transactional

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -11,19 +11,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
 
-    @Autowired
+    /*@Autowired
     PlatformTransactionManager transactionManager;
     TransactionStatus status;
 
@@ -31,7 +33,7 @@ class ItemRepositoryTest {
     void beforeEach() {
         // 트랜잭션 시작
         status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-    }
+    }*/
 
     @AfterEach
     void afterEach() {
@@ -40,7 +42,7 @@ class ItemRepositoryTest {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
         // 트랜잭션 롤백
-        transactionManager.rollback(status);
+        //transactionManager.rollback(status);
     }
 
     @Test
