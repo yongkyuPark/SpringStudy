7728dfd2ff0849180974c3abf4c7b1655c4b8d8a
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-05-09 23:04:49 +0900

    feat : JPA 적용

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,12 +1,17 @@
 package hello.itemservice.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
