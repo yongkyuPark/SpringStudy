fbe701ab96e23764a30d5109646fcf733080b5e3
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-01-14 21:15:40 +0900

    Revert "Revert "feat : 정리""
    This reverts commit 1db3fb661b924ea2aff2080172149d7dde5fba18.

--- aop/src/main/java/hello/aop/exam/ExamService.java
+++ aop/src/main/java/hello/aop/exam/ExamService.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ public class ExamService {
     private final ExamRepository examRepository;
 
     @Trace
+    @Retry(4)
     public void request(String itemId) {
         examRepository.save(itemId);
     }
