1db3fb661b924ea2aff2080172149d7dde5fba18
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-01-14 21:14:33 +0900

    Revert "feat : 정리"
    This reverts commit 140e5a4b2aa3cc1e0e2cd7cbbdc84ad43de673b8.

--- aop/src/main/java/hello/aop/exam/ExamService.java
+++ aop/src/main/java/hello/aop/exam/ExamService.java
@@ -1,6 +1,5 @@
 package hello.aop.exam;
 
-import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +11,6 @@ public class ExamService {
     private final ExamRepository examRepository;
 
     @Trace
-    @Retry(4)
     public void request(String itemId) {
         examRepository.save(itemId);
     }
--- aop/src/test/java/hello/aop/exam/ExamTest.java
+++ aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -1,6 +1,5 @@
 package hello.aop.exam;
 
-import hello.aop.exam.aop.RetryAspect;
 import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@Import({TraceAspect.class, RetryAspect.class})
+@Import(TraceAspect.class)
 @SpringBootTest
 public class ExamTest {
 
