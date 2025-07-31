44f489149db83827510c0d093141c8f7d135d352
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-07-31 20:59:41 +0900

    feat : DI와 단위 테스트

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -16,7 +16,9 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return helloService.sayHello(Objects.requireNonNull(name));
+        if(name == null || name.trim().length() == 0) throw new IllegalArgumentException();
+
+        return helloService.sayHello(name);
     }
 
 }
