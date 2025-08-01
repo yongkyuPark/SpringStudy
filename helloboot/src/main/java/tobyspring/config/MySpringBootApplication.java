a6c4f4254a00e886d2f2d0e797d0ee7bb3d184c5
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-08-01 20:00:52 +0900

    feat : 인프라 빈 구성 정보의 분리

--- helloboot/src/main/java/tobyspring/config/MySpringBootApplication.java
+++ helloboot/src/main/java/tobyspring/config/MySpringBootApplication.java
@@ -1,4 +1,4 @@
-package tobyspring.helloboot;
+package tobyspring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +12,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Configuration
 @ComponentScan
-public @interface MySpringBootAnnotation {
+@EnableMyAutoConfiguration
+public @interface MySpringBootApplication {
 }
