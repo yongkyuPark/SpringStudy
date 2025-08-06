6122e34232dd29ec9fe5cda65fcf36fd55091e74
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-08-06 20:44:22 +0900

    feat : JdbcTemplate과 트랜잭션 매니저 구성

---
+++ helloboot/src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,36 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HelloBootTest
+public class JdbcTemplateTest {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("insert into hello values(?, ?)", "Park", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)", "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    void insertAndQuery2() {
+        jdbcTemplate.update("insert into hello values(?, ?)", "Park", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)", "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+}
