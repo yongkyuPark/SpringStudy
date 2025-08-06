1c309367b1bef0b0fc0cd7450fe743a69f5d44d7
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-08-06 21:19:21 +0900

    feat : 리포지토리를 사용하는 HelloService

--- helloboot/src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
+++ helloboot/src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -14,11 +14,6 @@ public class HelloRepositoryTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @BeforeEach
-    void init() {
-        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
-    }
-
     @Test
     void findHelloFailed() {
         Assertions.assertThat(helloRepository.findHello("Park")).isNull();
---
+++ helloboot/src/test/java/tobyspring/helloboot/HelloServiceCountTest.java
@@ -0,0 +1,24 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.stream.IntStream;
+
+@HelloBootTest
+public class HelloServiceCountTest {
+    @Autowired
+    HelloService helloService;
+
+    @Autowired
+    HelloRepository helloRepository;
+
+    @Test
+    void sayHelloIncreaseCount() {
+        IntStream.rangeClosed(1, 10).forEach(count -> {
+            helloService.sayHello("Park");
+            Assertions.assertThat(helloRepository.countOf("Park")).isEqualTo(count);
+        });
+    }
+}
